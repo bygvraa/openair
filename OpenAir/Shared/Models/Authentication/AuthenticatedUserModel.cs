a412d0470531a8d4571161ae46aeab2ddaec11f6
mikkel <eaamiby@students.eaaa.dk>
2021-11-30 12:49:56 +0100

    Flyttet usermodels i deres egen mappe

--- OpenAir/Shared/Models/Authentication/AuthenticatedUserModel.cs
+++ OpenAir/Shared/Models/Authentication/AuthenticatedUserModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OpenAir.Shared.Models
+namespace OpenAir.Shared.Models.Authentication
 {
     public class AuthenticatedUserModel
     {
--- OpenAir/Shared/Models/Authentication/AuthenticationUserModel.cs
+++ OpenAir/Shared/Models/Authentication/AuthenticationUserModel.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OpenAir.Shared.Models
+namespace OpenAir.Shared.Models.Authentication
 {
     public class AuthenticationUserModel
     {
