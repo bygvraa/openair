aba21ed76efca67ed745faaec8c40ec653f34668
mikkel <eaamiby@students.eaaa.dk>
2021-11-30 09:50:15 +0100

    Tilføjet begyndelsen på auth til projektet

---
+++ OpenAir/Client/Authentication/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using OpenAir.Shared.Models;
+using System.Threading.Tasks;
+
+namespace OpenAir.Client.Authentication
+{
+    public interface IAuthenticationService
+    {
+        Task<AuthenticatedUserModel> Login(AuthenticationUserModel userForAuthentication);
+        Task Logout();
+    }
+}
--- OpenAir/Client/Program.cs
+++ OpenAir/Client/Program.cs
@@ -1,7 +1,10 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using OpenAir.Client.Authentication;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -16,6 +19,13 @@ namespace OpenAir.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddApiAuthorization();
+            builder.Services.AddOptions();
+            builder.Services.AddAuthenticationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
