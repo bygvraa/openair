24769c5e626ff3a650dc383c7513697a40d45c0b
Louise Ellegaard <louiseellegaard@MacBook-Pro-tilhrende-Louise.local>
2021-12-08 11:09:17 +0100

    ew

--- Server/Controllers/BilletController.cs
+++ Server/Controllers/BilletController.cs
@@ -1,45 +1,45 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using OpenAir.Shared.Models;
-using OpenAir.Server.Data.Repositories;
-using Microsoft.EntityFrameworkCore;
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using OpenAir.Shared.Models;
+//using OpenAir.Server.Data.Repositories;
+//using Microsoft.EntityFrameworkCore;
 
-namespace OpenAir.Server.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class BilletController : ControllerBase
-    {
-        private readonly IBilletRepository _service;
+//namespace OpenAir.Server.Controllers
+//{
+//    [ApiController]
+//    [Route("[controller]")]
+//    public class BilletController : ControllerBase
+//    {
+//        private readonly IBilletRepository _service;
 
-        public BilletController(IBilletRepository service)
-        {
-            _service = service;
-        }
-        // GET: /<controller>/
+//        public BilletController(IBilletRepository service)
+//        {
+//            _service = service;
+//        }
+//        // GET: /<controller>/
 
-        // GET: api/task
-        // Bruger en metode ('GetAllTasks()') fra 'TaskRepository' til at retunere en liste over alle opgaver
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<TaskClass>>> GetAll()
-        {
-            return await _service.GetAllBilletter();
-        }
+//        // GET: api/task
+//        // Bruger en metode ('GetAllTasks()') fra 'TaskRepository' til at retunere en liste over alle opgaver
+//        [HttpGet]
+//        public async Task<ActionResult<IEnumerable<TaskClass>>> GetAll()
+//        {
+//            return await _service.GetAllBilletter();
+//        }
 
 
-        [HttpGet("{billet_id}")]
-        public async Billet<ActionResult<BilletClass>> Get(int billet_id)
-        {
-            var billet = await _service.GetBillet(billet_id);
+//        [HttpGet("{billet_id}")]
+//        public async Billet<ActionResult<BilletClass>> Get(int billet_id)
+//        {
+//            var billet = await _service.GetBillet(billet_id);
 
-            if (billet == null)
-                return NotFound();
+//            if (billet == null)
+//                return NotFound();
 
-            return billet;
-        }
-    }
-}
+//            return billet;
+//        }
+//    }
+//}
--- Server/Data/Repositories/BilletRepository.cs
+++ Server/Data/Repositories/BilletRepository.cs
@@ -1,63 +1,63 @@
-﻿using OpenAir.Shared.Models;
-using OpenAir.Server.Data;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿//using OpenAir.Shared.Models;
+//using OpenAir.Server.Data;
+//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore;
+//using System;
 
-namespace OpenAir.Server.Data.Repositories
-{
-    public class BilletRepository : IBilletRepository
-    {
-        private readonly ApplicationDbContext _dBContext;
+//namespace OpenAir.Server.Data.Repositories
+//{
+//    public class BilletRepository : IBilletRepository
+//    {
+//        private readonly ApplicationDbContext _dBContext;
 
-        public BilletRepository(ApplicationDbContext dBContext)
-        {
-            _dBContext = dBContext;
-        }
+//        public BilletRepository(ApplicationDbContext dBContext)
+//        {
+//            _dBContext = dBContext;
+//        }
 
 
-        // Task -----------------------------------------
+//        // Task -----------------------------------------
 
-        // GET    - find alle opgaver
-        public async Billet<List<BilletClass>> GetAllBilletter()
-        {
-            return await _dBContext.task.ToListAsync();
-        }
+//        // GET    - find alle opgaver
+//        public async Billet<List<BilletClass>> GetAllBilletter()
+//        {
+//            return await _dBContext.task.ToListAsync();
+//        }
 
-        // GET    - find specifik opgaver
-        public async Task<TaskClass> GetTask(int task_id)
-        {
+//        // GET    - find specifik opgaver
+//        public async Task<TaskClass> GetTask(int task_id)
+//        {
 
-            return await _dBContext.task.FindAsync(task_id);
-        }
+//            return await _dBContext.task.FindAsync(task_id);
+//        }
 
-        // POST   - lav en opgave
-        public async Task CreateTask(TaskClass task)
-        {
-            await _dBContext.task.AddAsync(task);
-            await _dBContext.SaveChangesAsync();
-        }
+//        // POST   - lav en opgave
+//        public async Task CreateTask(TaskClass task)
+//        {
+//            await _dBContext.task.AddAsync(task);
+//            await _dBContext.SaveChangesAsync();
+//        }
 
-        // PUT    - opdater en opgave
-        public async Task UpdateTask(TaskClass task)
-        {
-            _dBContext.task.Update(task);
-            await _dBContext.SaveChangesAsync();
-        }
+//        // PUT    - opdater en opgave
+//        public async Task UpdateTask(TaskClass task)
+//        {
+//            _dBContext.task.Update(task);
+//            await _dBContext.SaveChangesAsync();
+//        }
 
-        // DELETE - fjern en opgave
-        public async Task DeleteTask(int task_id)
-        {
-            var taskToDelete = await _dBContext.task.FindAsync(task_id);
+//        // DELETE - fjern en opgave
+//        public async Task DeleteTask(int task_id)
+//        {
+//            var taskToDelete = await _dBContext.task.FindAsync(task_id);
 
-            if (taskToDelete == null)
-                throw new NullReferenceException();
+//            if (taskToDelete == null)
+//                throw new NullReferenceException();
 
-            _dBContext.task.Remove(taskToDelete);
-            await _dBContext.SaveChangesAsync();
-        }
+//            _dBContext.task.Remove(taskToDelete);
+//            await _dBContext.SaveChangesAsync();
+//        }
 
-        // -------------------------------------------------
-    }
-}
+//        // -------------------------------------------------
+//    }
+//}
