f85e198818cf95d8fc31da0cb640885316457925
Kenny Au <kennyau@MacBook-Pro-tilhrende-Kenny.local>
2021-12-08 11:08:48 +0100

    vietnam

--- Server/Controllers/TicketController.cs
+++ Server/Controllers/TicketController.cs
@@ -8,17 +8,19 @@ using System.IO;
 using OpenAir.Shared;
 using Microsoft.Extensions.Logging;
 using OpenAir.Shared.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace OpenAir.Server.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [AllowAnonymous]
     public class TicketController : ControllerBase
     {
         [HttpGet]
         public IEnumerable<TicketClass> Get()
         {
-            using (StreamReader r = new StreamReader("/tickets.json"))
+            using (StreamReader r = new StreamReader("tickets.json"))
             {
                 string json = r.ReadToEnd();
                 List<TicketClass> items = JsonConvert.DeserializeObject<List<TicketClass>>(json);
