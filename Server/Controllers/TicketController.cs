6fa156ea8376fd1b14424674d543241a3fb7a641
Kenny Au <kennyau@MacBook-Pro-tilhrende-Kenny.local>
2021-12-08 10:24:04 +0100

    vietnam

---
+++ Server/Controllers/TicketController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IO;
+using OpenAir.Shared;
+using Microsoft.Extensions.Logging;
+using OpenAir.Shared.Models;
+
+namespace OpenAir.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TicketController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<TicketClass> Get()
+        {
+            using (StreamReader r = new StreamReader("/tickets.json"))
+            {
+                string json = r.ReadToEnd();
+                List<TicketClass> items = JsonConvert.DeserializeObject<List<TicketClass>>(json);
+                return items;
+            }
+        }
+    }
+}
+
---
+++ Shared/Models/TicketClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAir.Shared.Models
+{
+    public class TicketClass
+    {
+        public string Title { get; set; }
+        public string Date { get; set; }
+        public int Pris { get; set; }
+    }
+}
