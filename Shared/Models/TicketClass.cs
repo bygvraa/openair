f83d951a3b41ef38766624561df43bdda48ef549
Kenny Au <kennyau@MacBook-Pro-tilhrende-Kenny.local>
2021-12-13 19:00:13 +0100

    LOUISE ER CHEF

--- Shared/Models/TicketClass.cs
+++ Shared/Models/TicketClass.cs
@@ -11,13 +11,15 @@ namespace OpenAir.Shared.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public int Price { get; set; }
+        public string Email { get; set; }
 
         public TicketClass() { }
-        public TicketClass(int Id, string Title, int Price)
+        public TicketClass(int Id, string Title, int Price, string Email)
         {
             this.Id = Id;
             this.Title = Title;
             this.Price = Price;
+            this.Email = Email;
         }
     }
 }
