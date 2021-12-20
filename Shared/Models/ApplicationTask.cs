d32fb2f517fab61217e03aa381e51446b3c39bdf
Mikkel <eaamiby@students.eaaa.dk>
2021-12-20 07:41:51 +0100

    Opdateres css

--- Shared/Models/ApplicationTask.cs
+++ Shared/Models/ApplicationTask.cs
@@ -34,7 +34,13 @@ namespace OpenAir.Shared.Models
         public DateTime GetModified() { return Modified; }
     }
 
-    // Enum
+    // Enums
+    public enum UserType
+    {
+        Frivillig,
+        Kontaktperson
+    }
+
     public enum Category
     {
         Sceneopsætning,
